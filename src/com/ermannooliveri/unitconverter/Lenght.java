914d0795e704822e7d31259d65a2b68472ae05db
ErmannoOliveri <58082615+ErmannoOliveri@users.noreply.github.com>
2022-01-07 19:25:17 +0100

    Update Lenght.java

--- src/com/ermannooliveri/unitconverter/Lenght.java
+++ src/com/ermannooliveri/unitconverter/Lenght.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Ermanno Oliveri
+ * Copyright 2022 Ermanno Oliveri
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
