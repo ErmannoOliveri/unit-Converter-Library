e506d451ddbeb60fb12a1b8bcecaaa81c3ebf1d6
ErmannoOliveri <58082615+ErmannoOliveri@users.noreply.github.com>
2022-02-17 16:47:11 +0100

    Add files via upload

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.ermannooliveri.unitconverter","l":"Lenght"},{"p":"com.ermannooliveri.unitconverter","l":"Main"},{"p":"com.ermannooliveri.unitconverter","l":"Mass"},{"p":"com.ermannooliveri.unitconverter","l":"Surface"}];updateSearchResults();
